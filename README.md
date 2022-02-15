c159fccddd9457511278849cc6e71d52847eab8d
ZakharenkoIV <zaharenkoiv93@gmail.com>
2022-02-15 20:42:44 +0300

    0. RESTFull. Описание архитектуры [#6884]

---
+++ README.md
@@ -0,0 +1,9 @@
+##  Описание ##
+Это учебный проект. Практика написания REST-сервисов
+####  Сервис Работники #### 
+REST-сервис сотрудников. Реализованы CRUD операции, написаны unit тесты.
+####  Технологии проекта ####
+![ значок ](https://img.shields.io/badge/PostgreSQL-13-blue)
+![ значок ](https://img.shields.io/badge/Java-17-green)
+![ значок ](https://img.shields.io/badge/Maven-3.6-green)
+![ значок ](https://img.shields.io/badge/SpringBot-2.6-yellow)
---
+++ src/test/java/ru/job4j/controller/PersonControllerTest.java
@@ -0,0 +1,101 @@
+package ru.job4j.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.Job4jAuthApplication;
+import ru.job4j.domain.Person;
+import ru.job4j.repository.PersonRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.StringJoiner;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(classes = Job4jAuthApplication.class)
+@AutoConfigureMockMvc
+public class PersonControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PersonRepository persons;
+
+    @Test
+    public void whenGetFindAllPersonsThenReturnAllPersonsList() throws Exception {
+        List<Person> allPersons = List.of(
+                Person.of(1, "parsentev", "123"),
+                Person.of(2, "ban", "123"),
+                Person.of(3, "ivan", "123")
+        );
+        Mockito.when(persons.findAll()).thenReturn(allPersons);
+        StringJoiner foundAllPersons = new StringJoiner(",", "[", "]");
+        foundAllPersons.add("{\"id\":1,\"login\":\"parsentev\",\"password\":\"123\"}");
+        foundAllPersons.add("{\"id\":2,\"login\":\"ban\",\"password\":\"123\"}");
+        foundAllPersons.add("{\"id\":3,\"login\":\"ivan\",\"password\":\"123\"}");
+        this.mockMvc.perform(get("/person/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().string(foundAllPersons.toString()));
+    }
+
+    @Test
+    public void whenGetPersonByIdThenReturnPerson() throws Exception {
+        Mockito.when(persons.findById(2)).thenReturn(
+                Optional.of(Person.of(2, "ban", "123")));
+        String foundPerson = "{\"id\":2,\"login\":\"ban\",\"password\":\"123\"}";
+        this.mockMvc.perform(get("/person/{id}", 2))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().string(foundPerson));
+    }
+
+    @Test
+    public void whenSavePersonThenReturnThisPerson() throws Exception {
+        Person person = Person.of(4, "tom", "123");
+        Mockito.when(persons.save(any(Person.class))).thenReturn(person);
+        String savedPerson = "{\"id\":4,\"login\":\"tom\",\"password\":\"123\"}";
+        this.mockMvc.perform(post("/person/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(savedPerson))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().string(savedPerson));
+    }
+
+    @Test
+    public void whenPutPersonThenReturnStatusOk() throws Exception {
+        Person person = Person.of(2, "tom", "123");
+        Mockito.when(persons.save(any(Person.class))).thenReturn(person);
+        String savedPerson = "{\"id\":2,\"login\":\"tom\",\"password\":\"123\"}";
+        this.mockMvc.perform(put("/person/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(savedPerson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+
+    @Test
+    public void whenDeletePersonThenReturnStatusOk() throws Exception {
+        doNothing().when(persons).delete(Person.of(2, null, null));
+        this.mockMvc.perform(delete("/person/{id}", 2))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+}
